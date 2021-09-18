effde2ef8a6df4656b35596a140bb1e2dd4eab6f
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-18 16:10:55 -0300

    calculadora com bug no calculo

---
+++ ch2/calculadora_idade_canina.py
@@ -0,0 +1,9 @@
+prompt = 'Olá, qual é o nome do seu dog?'
+dog_name = input(prompt)
+
+prompt = 'legal, e qual é a idade dele?'
+dog_age = input(prompt)
+
+age = dog_age * (7)
+
+print ('Seu dog ' + dog_name + ' tem ' + age + ' em anos humanos!' )
