8d134ccb13e20a29c06b75ce9c9de088f9c94e1e
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-18 16:55:32 -0300

    refactoring

--- ch2/calculadora_idade_canina.py
+++ ch2/calculadora_idade_canina.py
@@ -1,7 +1,7 @@
 dog_name = input('Olá, qual é o nome do seu dog? ')
 dog_age = input('legal, e qual é a idade dele? ' )
 human_age = int(dog_age) * 7
-frase_retorno = ('A idade do ' + dog_name + ' em anos humanos é ' )
+human_age_dog = str(human_age)
+frase_retorno = ('A idade do ' + dog_name + ' em anos humanos é ' + human_age_dog + '!')
 
 print (frase_retorno)
-print (human_age)
