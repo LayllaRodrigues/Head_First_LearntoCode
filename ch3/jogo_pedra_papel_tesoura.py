3d41de66e2dff102b6906ff90341d408fbef206f
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-18 17:59:19 -0300

    jogo papel, pedra e tesoura

--- ch3/jogo_pedra_papel_tesoura.py
+++ ch3/jogo_pedra_papel_tesoura.py
@@ -1,6 +1,17 @@
 import random
 
-random_choice = random.randint(0,2)
-print ('The computer chooses', random_choice)
+random_choice = random.randint(0 , 2)
 
+if random_choice == 0:
+    escolha_do_computador = 'Pedra'
+elif random_choice == 1:
+    escolha_do_computador = 'Papel'
+else:
+    escolha_do_computador = 'Tesoura'
+    
+    
+escolha_do_usuario = input('Pedra, Papel ou Tesoura?' )
+
+print('Vc escolheu ', escolha_do_usuario, 'e o computador escolheu ', escolha_do_computador)
+    
 
