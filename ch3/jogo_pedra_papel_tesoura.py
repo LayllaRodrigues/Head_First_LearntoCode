ea1d3aa6240a65aeca1001a47e573198c88984a1
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-18 18:40:51 -0300

    incluindo vencedor

--- ch3/jogo_pedra_papel_tesoura.py
+++ ch3/jogo_pedra_papel_tesoura.py
@@ -4,8 +4,21 @@ escolhas = ['Pedra', 'Papel', 'Tesoura']
 escolha_do_computador= random.choice(escolhas)
 
     
-escolha_do_usuario = input('Pedra, Papel ou Tesoura?' )
+escolha_do_usuario = input('Pedra, Papel ou Tesoura? ' )
 
-print('Vc escolheu ', escolha_do_usuario, 'e o computador escolheu ', escolha_do_computador)
-    
+if escolha_do_computador == escolha_do_usuario:
+    vencedor = 'Empate'
+elif escolha_do_computador == 'Papel' and escolha_do_usuario == 'Pedra':
+    vencedor = 'Computer'
+elif escolha_do_computador == 'Pedra' and escolha_do_usuario == 'Tesoura':
+    vencedor = 'Computer'
+elif escolha_do_computador == 'Tesoura' and escolha_do_usuario == 'Papel':
+    vencedor = 'Computer'
+else:
+    vencedor = 'Usuario'
+
+if vencedor == 'Empate':
+    print('Escolhemos igual !', escolha_do_computador + ', Jogue novamente.')
+else:
+    print('O ' + vencedor , 'venceu!', ' o computador escolheu ' + escolha_do_computador)
 
