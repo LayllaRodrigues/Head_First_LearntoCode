04c649106c6fe2ec727ee89746bead1feea33987
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-18 14:26:57 -0300

    exercicio traduzindo em codigo

---
+++ ch1/traduzindo.py
@@ -0,0 +1,20 @@
+#Exercicio:
+#Traduza as frazes em códigos 
+
+# print "hi there" on the creen.
+print ('Hi there')
+
+#if the temperature is more than 72, then print "Wear shorts" on the screen 
+if temperature > 72: 
+        print('Wear shorts')
+
+
+#A grocery list with bread, milk, and eggs on it.
+grocery_list = ['bread', 'milk', 'eggs']
+
+#Ask the user, "Whats is your name?"
+name = input ('Whats is your name?')
+
+#Pouring five drinks.
+for num in range (0,5):
+    pour_drink()
