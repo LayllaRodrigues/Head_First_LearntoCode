c454e6d15f2e6477ea6259022b42314d50b31192
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-19 14:57:56 -0300

    usando funcao len

---
+++ ch4/arrays.py
@@ -0,0 +1,32 @@
+#listar elementos em array
+
+smoothies = ['coconut', 'strawberry', 'banana', 'pineapple', 'açai berry']
+
+
+#Como acessar elementos da lista
+#Cada elemento da lista possui um indice, e essa é a chave para acessar os valores em uma lista.
+
+#EXEMPLO:
+
+favorites = smoothies[4]
+
+#Tambem é possivel alterar o valor de um item na lista usando o indice
+
+smoothies[3] = 'tropical'
+
+
+#QUÃO GRANDE É ESSA LISTA?
+
+    #Digamos que alguem lhe entregue uma lista grande com dados, vc sabe como obter o tamanho dela? 
+    #O python fornece uma função integrada oara informa-lo, ela é chamada de LEN 
+    
+#EXEMPLO DE COMO USAR FUNCAO LEN:
+
+length = len (smoothies)
+
+#Imprimindo valores
+
+print   (length)
+print   (smoothies)
+print   (favorites)
+
