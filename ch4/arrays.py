f70ccf1cbbd8fbf2d75cabec838b6e4235c5fcf4
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-19 15:14:06 -0300

    acessando ultimo elemento da lista

--- ch4/arrays.py
+++ ch4/arrays.py
@@ -30,3 +30,34 @@ print   (length)
 print   (smoothies)
 print   (favorites)
 
+#Acessando ultimo item da lista: as listas geralmente têm dados organizados com 
+# os valores mais recentes, e geralmente os mais importantes, no final (ou seja, 
+# no maior índice), portanto, acessar o último item da lista é uma tarefa comum.
+
+#Para obter o último item de nossa lista de smoothies, fazemos o seguinte:
+
+length = len(smoothies)
+last = smoothies[length-1]
+print(last)
+
+#Python oferece outra forma de acessar o ultimo item da lista:
+#você pode usar um índice negativo, começando em –1, para especificar os itens 
+# em uma lista na ordem inversa. Portanto, um índice de –1é o último item da lista,
+# um índice de –2é o penúltimo e assim por diante.
+
+#EXEMPLO:
+last = smoothies[-1]
+second_last = smoothies[-2]
+third_last = smoothies[-3]
+print(last)
+print(second_last)
+print(third_last)
+
+
+
+
+
+
+
+
+
