440ae68e42ce81d225c1e85f752dde9c4657df69
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-19 21:59:58 -0300

    funcao

---
+++ ch5/abstracao.py
@@ -0,0 +1,17 @@
+# definição de uma função
+# para criar uma função, comece com a palavra-chave def e a seguir com um nome para lembrar a função
+# em seguida, você pode ter zero ou mais parâmetros. pense nisso como variáveis ​​que contêm valores que você passará para a função ao chamá-la
+# e então temos dois pontos, que, como você sabe, inicia um bloco de código em python
+# aqui está o bloco de código que vamos reutilizar
+# em python e em quase todas as linguagens, chamamos esse bloco de código de corpo da função
+
+def bark (name, weight):
+    if weight > 20:
+        print  (name, 'says WOOF WOOF')
+    else:
+        print(name, 'says woolf, woolf')
+        
+bark('Codie', 40)
+bark('Sparky', 9)
+bark('Jackson', 12)
+bark('Fido', 65)
