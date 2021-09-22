110766398420fb2fddcaee0651e6431b4e561c3d
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-09-22 00:07:25 -0300

    argumentos key-word

--- ch5/variaveis_globais.py
+++ ch5/variaveis_globais.py
@@ -35,4 +35,19 @@ greet('june')
 
 greet('june', 'nice see you!')
 
+#se sua função tem um parâmetro sem um valor padrão, então quando sua função é chamada, 
+# ela deve fornecer um argumento para aquele parâmetro, então é obrigatório. 
+# Então, digamos que deveríamos expandir nossa greetfunção assim:
 
+def greet(name, emoticon, message = 'You rule!'):
+    print('Hi', name + '. ', message, emoticon)
+
+
+#Até agora, sempre que chamamos uma função, os argumentos foram posicionais . 
+# Ou seja, o primeiro argumento é mapeado para o primeiro parâmetro,
+# o segundo argumento para o segundo parâmetro e assim por diante. 
+# Você também pode usar os nomes dos parâmetros como palavras-chave e especificar os argumentos 
+# em uma ordem diferente, se desejar.
+
+#EXEMPLO:
+greet(message='hi', name='jay', emoticon= 'thumbs up')
